4501468be287ba585d5c9628fb3e9be9f0014a88
MrCipher691 <ayush.ram691@gmail.com>
2020-09-30 18:20:21 +0530

    List Update

--- MPU_Raw_Data_Grapher.py
+++ MPU_Raw_Data_Grapher.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import csv
 
-x=[]
+time=[]
 yaccx=[]
 yaccy=[]
 yaccz=[]
@@ -12,7 +12,7 @@ ygyroz=[]
 with open('MPURawData_0.csv','r') as csvfile:
     plots = csv.reader(csvfile, delimiter=',')
     for row in plots:
-        x.append(float(row[0]))
+        time.append(float(row[0]))
         yaccx.append(float(row[1]))
         yaccy.append(float(row[2]))
         yaccz.append(float(row[3]))
@@ -23,22 +23,22 @@ with open('MPURawData_0.csv','r') as csvfile:
 fig, axs = plt.subplots(2, 3)
 fig.suptitle('MPU-6050 RAW DATA')
 
-axs[0, 0].plot(x, yaccx, 'tab:red')
+axs[0, 0].plot(time, yaccx, 'tab:red')
 axs[0, 0].set_title('Accel: X-Axis')
 
-axs[0, 1].plot(x, yaccy, 'tab:green')
+axs[0, 1].plot(time, yaccy, 'tab:green')
 axs[0, 1].set_title('Accel: Y-Axis')
 
-axs[0, 2].plot(x, yaccz, 'tab:blue')
+axs[0, 2].plot(time, yaccz, 'tab:blue')
 axs[0, 2].set_title('Accel: Z-Axis')
 
-axs[1, 0].plot(x, ygyrox, 'tab:red')
+axs[1, 0].plot(time, ygyrox, 'tab:red')
 axs[1, 0].set_title('Gyro: X-Axis')
 
-axs[1, 1].plot(x, ygyroy, 'tab:green')
+axs[1, 1].plot(time, ygyroy, 'tab:green')
 axs[1, 1].set_title('Gyro: X-Axis')
 
-axs[1, 2].plot(x, ygyroz, 'tab:blue')
+axs[1, 2].plot(time, ygyroz, 'tab:blue')
 axs[1, 2].set_title('Gyro: X-Axis')
 
 for ax in axs.flat:
