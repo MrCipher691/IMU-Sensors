e7105c8137ef97e4b66d5cfd5a7007fedc5f8cbd
MrCipher691 <ayush.ram691@gmail.com>
2020-12-24 18:37:56 +0530

    Interactive Graphs

--- MPU_Raw_Data_Grapher.py
+++ MPU_Raw_Data_Grapher.py
@@ -1,15 +1,24 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+from pandas_datareader import data, wb
+import plotly.graph_objs as go
 
-df_mpu = pd.read_csv(r'MPURawData_0.csv')
+df = pd.read_csv(r'MPURawData_0.csv')
 
-ax = plt.gca()
+fig = go.Figure()
 
-df_mpu.plot(kind='line', x='time', y='acceleration x',color='red', ax=ax)
-df_mpu.plot(kind='line', x='time', y='acceleration y',color='orange', ax=ax)
-df_mpu.plot(kind='line', x='time', y='acceleration z',color='yellow', ax=ax)
-df_mpu.plot(kind='line', x='time', y='gyroscope x',color='blue', ax=ax)
-df_mpu.plot(kind='line', x='time', y='gyroscope y',color='purple', ax=ax)
-df_mpu.plot(kind='line', x='time', y='gyroscope z',color='black', ax=ax)
+fig.add_trace(go.Scatter(x=df['time'], y=df['acceleration x'], name='ACCEL X'))
+fig.add_trace(go.Scatter(x=df['time'], y=df['acceleration y'], name='ACCEL Y'))
+fig.add_trace(go.Scatter(x=df['time'], y=df['acceleration z'], name='ACCEL Z'))
+fig.add_trace(go.Scatter(x=df['time'], y=df['gyroscope x'], name='GYRO X'))
+fig.add_trace(go.Scatter(x=df['time'], y=df['gyroscope y'], name='GYRO Y'))
+fig.add_trace(go.Scatter(x=df['time'], y=df['gyroscope z'], name='GYRO Z'))
+fig.update_xaxes(rangeslider_visible=True)
 
-plt.show()
+fig.update_layout(
+    title="MPU6050 - RAW DATA",
+    xaxis_title="TIME",
+    yaxis_title="g & g/s",
+    legend_title="DATA TYPE"
+)
+
+fig.show()
